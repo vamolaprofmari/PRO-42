fafc050ee6cc8eced9e718bbe708f166924c579c
vamolaprofmari <123978185+vamolaprofmari@users.noreply.github.com>
2023-06-05 19:11:42 -0300

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -7,7 +7,7 @@ class Form {
   }
 
   setElementsPosition() {
-    tthis.titleImg.position(width / 2 -600 , height / 2 -200);
+    this.titleImg.position(width / 2 -600 , height / 2 -200);
     this.input.position(width / 2 , height / 2 +200 );
     this.playButton.position(width / 2 , height / 2 +300);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
